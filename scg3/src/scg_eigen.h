fb4250a04e70f736ea1694931f9de3256b81c50d
Henrik Masbruch <henrik.masbruch@tum.de>
2016-09-19 09:56:04 +0200

    Added Eigen library

---
+++ scg3/src/scg_eigen.h
@@ -0,0 +1,9 @@
+#ifndef SCG_EIGEN_H_
+#define SCG_EIGEN_H_
+
+#include "../extern/Eigen/include/Eigen/Core"
+#include "../extern/Eigen/include/Eigen/LU"
+#include "../extern/Eigen/include/Eigen/Eigenvalues"
+
+
+#endif /* SCG_EIGEN_H_ */
